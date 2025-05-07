ac336e0cb9f6308e9ccfc042b5bfc2e03774b87c
AyushiR0y <ayushiroy283@gmail.com>
2025-05-08 00:06:43 +0530

    Update greeting message in user enhancement branch

--- order-service/app/src/main/java/org/example/App.java
+++ order-service/app/src/main/java/org/example/App.java
@@ -5,7 +5,7 @@ package org.example;
 
 public class App {
     public String getGreeting() {
-        return "Hello World!";
+        return "Hello World! This is the User enhancement module";
     }
 
     public static void main(String[] args) {
